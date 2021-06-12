deef4dbc6df5afcdd446d96ae80dfdf404a7d24f
unknown <sepsad@cafebazaar.ir>
2021-06-12 13:48:18 +0430

    DAU page is edited!

--- apps/DAU.py
+++ apps/DAU.py
@@ -7,18 +7,21 @@ import plotly.express as px
 from utils import read_dau_percent_data, read_dau_data
 
 
-df_dau_percent = read_dau_percent_data()
-df_dau = read_dau_data()
+dau_percent_df = read_dau_percent_data()[['startDate','day_number_from_install', 'size']]
+dau_df = read_dau_data()[['startDate','day_number_from_install', 'size']]
+
+DAU = pd.merge(dau_df, dau_percent_df, on= ['startDate','day_number_from_install'])
+
+DAU.columns = ['startDate','day_number_from_install', 'size', 'percent']
 
 def display_fig():
-    fig_2b = px.line(df_dau_percent, x='startDate', y= 'size', color = 'day_number_from_install', title= 'DAU percent per day')
-    fig_2c = px.line(df_dau, x='startDate', y= 'size', color = 'day_number_from_install', title= 'DAU size per day')
+    fig_2b = px.line(DAU, x='startDate', y= 'percent', color = 'day_number_from_install',hover_data=['size'], title= 'DAU percent per day')
 
-    return fig_2b, fig_2c
-
+    return fig_2b
 
 
-fig_2b, fig_2c = display_fig()
+
+fig_2b = display_fig()
 
 
 layout = dbc.Container([
@@ -28,11 +31,6 @@ layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dcc.Graph(id = "fig-DAU-percent", figure = fig_2b)],
-             width={"size": 5}) 
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id = "fig-DAU", figure = fig_2c)],
              width={"size": 5}) 
     ])
 ])
